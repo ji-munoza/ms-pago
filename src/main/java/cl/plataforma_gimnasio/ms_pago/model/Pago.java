6acd07ff3a35cfc82a62de0f849e78c4cd800ca5
ji-munoza <ji.munoza@duocuc.cl>
2026-05-17 21:32:46 -0400

    creado model pago

---
+++ src/main/java/cl/plataforma_gimnasio/ms_pago/model/Pago.java
@@ -0,0 +1,32 @@
+package cl.plataforma_gimnasio.ms_pago.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "pagos")
+public class Pago {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_pago")
+    private Integer idPago;
+
+    @Column(name = "monto", nullable = false)
+    private Integer montoPago;
+
+    @Column(name = "metodo_pago", nullable = false, length = 50)
+    private String metodoPago;
+
+    @Column(name = "estado_pago", nullable = false, length = 50)
+    private String estadoPago;
+
+    @Column(name = "fecha_hora_pago", nullable = false)
+    private LocalDateTime fechaHoraPago;
+}
